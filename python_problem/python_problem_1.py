6af9f6944bfd15111cac6e469fc3ec5173db4685
OH-GITAEK <dhlrxor1512@gmail.com>
2024-01-05 15:02:09 +0900

    5단계 수정

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,23 +3,43 @@ orderA = 1
 
 
 
-while(num < 31):
-  #n입력받기
-  n = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-  orderA += 1
-  while(n != '1' and n != '2' and n != '3'):
-    if(not(n.isdigit())):
-      n = input("정수를 입력하세요 : ")
-    else:
-      n = input("1, 2, 3 중 하나를 입력하세요 : ")
-  
-  #n입력 받은만큼 숫자 출력
-  if(orderA%2 ==0):
-    for i in range(int(n)):
-      num += 1
-      print("playerA : {}".format(num))
+
+#n입력받기 A순서
+n = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+orderA += 1
+while(n != '1' and n != '2' and n != '3'):
+  if(not(n.isdigit())):
+    n = input("정수를 입력하세요 : ")
   else:
-    for i in range(int(n)):
-      num += 1
-      print("playerB : {}".format(num))
-  
+    n = input("1, 2, 3 중 하나를 입력하세요 : ")
+
+#n입력 받은만큼 숫자 출력
+if(orderA%2 ==0):
+  for i in range(int(n)):
+    num += 1
+    print("playerA : {}".format(num))
+else:
+  for i in range(int(n)):
+    num += 1
+    print("playerB : {}".format(num))
+
+
+#n입력받기 B순서
+n = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+orderA += 1
+while(n != '1' and n != '2' and n != '3'):
+  if(not(n.isdigit())):
+    n = input("정수를 입력하세요 : ")
+  else:
+    n = input("1, 2, 3 중 하나를 입력하세요 : ")
+
+#n입력 받은만큼 숫자 출력
+if(orderA%2 ==0):
+  for i in range(int(n)):
+    num += 1
+    print("playerA : {}".format(num))
+else:
+  for i in range(int(n)):
+    num += 1
+    print("playerB : {}".format(num))
+
