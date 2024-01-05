67cbc311d6487c3296fab6d5ec31a72207c91901
OH-GITAEK <dhlrxor1512@gmail.com>
2024-01-05 15:16:45 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,32 +1,40 @@
+import random 
+
 num = 0
-orderA = 1
+orderA = 0
+
 
 def brGame():
   global num
   global orderA
-  n = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
   orderA += 1
-  while(n != '1' and n != '2' and n != '3'):
-    if(not(n.isdigit())):
-      n = input("정수를 입력하세요 : ")
-    else:
-      n = input("1, 2, 3 중 하나를 입력하세요 : ")
+  if(orderA%2 ==0):
+    n = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    while(n != '1' and n != '2' and n != '3'):
+      if(not(n.isdigit())):
+        n = input("정수를 입력하세요 : ")
+      else:
+        n = input("1, 2, 3 중 하나를 입력하세요 : ")
+  else:
+    n = random.randint(1,3)
   
+
   #n입력 받은만큼 숫자 출력
   if(orderA%2 ==0):
     for i in range(int(n)):
       num += 1
       print("playerA : {}".format(num))
       if(num == 31):
-        print("playerB win!")
+        print("computer win!")
         break
   else:
     for i in range(int(n)):
       num += 1
-      print("playerB : {}".format(num))
+      print("computer : {}".format(num))
       if(num == 31):
-        print("playerA win!")
-        break
+        print("player win!")
+        break 
+  
 
 
 while(num < 31):
